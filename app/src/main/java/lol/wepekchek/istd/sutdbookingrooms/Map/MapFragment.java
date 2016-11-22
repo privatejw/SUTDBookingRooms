92c8f9930f501811f5444cc777f9129a8adfb8b4
privatejw <privatejw@gmail.com>
2016-11-22 23:23:25 +0800

    added map overlay, map click, map bounds

--- app/src/main/java/lol/wepekchek/istd/sutdbookingrooms/Map/MapFragment.java
+++ app/src/main/java/lol/wepekchek/istd/sutdbookingrooms/Map/MapFragment.java
@@ -4,38 +4,38 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import lol.wepekchek.istd.sutdbookingrooms.R;
 
-import com.google.android.gms.plus.PlusOneButton;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnCameraIdleListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
+import com.google.android.gms.maps.model.LatLng;
 
 
-public class MapFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    // The request code must be 0 or greater.
-    private static final int PLUS_ONE_REQUEST_CODE = 0;
-    // The URL to +1.  Must be a valid URL.
-    private final String PLUS_ONE_URL = "http://developer.android.com";
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    private PlusOneButton mPlusOneButton;
+public class MapFragment extends Fragment implements OnCameraIdleListener, OnMapReadyCallback,
+        GoogleMap.OnMapClickListener {
+    GoogleMap mMap;
+    EditText txtMapSearch;
+    GroundOverlay mGroundOverlay;
 
-    public MapFragment() {
-        // Required empty public constructor
-    }
+    public MapFragment() {}
 
     public static MapFragment newInstance(String param1, String param2) {
         MapFragment fragment = new MapFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -43,10 +43,10 @@ public class MapFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
     }
 
     @Override
@@ -55,9 +55,34 @@ public class MapFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
-        //Find the +1 button
-        mPlusOneButton = (PlusOneButton) view.findViewById(R.id.plus_one_button);
+        txtMapSearch = (EditText) view.findViewById(R.id.txtMapSearch);
+
+        // Need to use getChildFragmentManager() if it is a nested fragment
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.sutdMap);
+        mapFragment.getMapAsync(this);
 
         return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        googleMap.setOnCameraIdleListener(this);
+        googleMap.setOnMapClickListener(this);
+        mGroundOverlay = googleMap.addGroundOverlay(new GroundOverlayOptions()
+                .image(BitmapDescriptorFactory.fromResource(R.drawable.newark_prudential_sunny))
+                .anchor(0, 1)
+                .position(new LatLng(37.421976, -122.084065), 8600f, 6500f));
+    }
+
+    @Override
+    public void onCameraIdle() {
+        txtMapSearch.setText(mMap.getCameraPosition().toString());
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        Toast.makeText(getActivity(), latLng.latitude+", "+latLng.longitude, Toast.LENGTH_SHORT).show();
     }
 }
