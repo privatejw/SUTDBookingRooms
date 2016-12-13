abd927543e51d4943935eb41110abc38d639da11
limyingjie <limyingjie@hotmail.com>
2016-12-14 05:47:58 +0800

    early opening hours lol

--- app/src/main/java/lol/wepekchek/istd/sutdbookingrooms/RoomSearch/CalendarFragment.java
+++ app/src/main/java/lol/wepekchek/istd/sutdbookingrooms/RoomSearch/CalendarFragment.java
@@ -55,7 +55,7 @@ public class CalendarFragment extends Fragment {
     private static String fromMap="";
     private static final String[] monthNumberToWord={"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug"
             ,"Sep","Oct","Nov","Dec"};
-    private static final String[] officeHours={"0800","0900","1000","1100","1200","1300","1400",
+    private static final String[] officeHours={"0500","0600","0700","0800","0900","1000","1100","1200","1300","1400",
             "1500","1600","1700","1800","1900","2000","2100"};
     private static final ArrayList<String> rooms = MapDatabase.listOfRooms;
     private Spinner spinner;
